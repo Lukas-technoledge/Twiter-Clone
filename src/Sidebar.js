120de11831adae0c06c7a00a6ee01b42a3bb4348
Lukas-technoledge <chialukaonuoha@gmail.com>
2022-06-25 04:47:43 -0700

    working on the sidebar component

---
+++ src/Sidebar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Sidebar.css'
+import TwitterIcon from '@mui/icons-material/Twitter';
+import SidebarOption from './SidebarOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import PeopleOutline from '@mui/icons-material/PeopleOutline';
+
+function Sidebar() {
+  return (
+    <div>
+      
+        { /*twittwer icon */ }
+        <TwitterIcon />
+        <SidebarOption Icon={HomeIcon} text='Home'/>
+        <SidebarOption Icon={SearchIcon} text='Explore'/>
+        <SidebarOption Icon={PeopleOutlineIcon} text='Communities'/>
+        <SidebarOption Icon={NotificationsNoneIcon} text='Notifications'/>
+        <SidebarOption Icon={MailOutlineIcon} text='Messages'/>
+        <SidebarOption Icon={BookmarkBorderIcon} text='Bookmarks'/>
+        <SidebarOption Icon={PermIdentityIcon} text='Profile'/>
+        <SidebarOption Icon={MoreHorizIcon} text='More'/>
+
+
+        { /*button tweet */ }
+
+    </div>
+  )
+}
+
+export default Sidebar
