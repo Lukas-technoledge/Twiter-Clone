bf3a1f3eac4c2001382b1c721d87aa76983aa30a
Lukas-technoledge <chialukaonuoha@gmail.com>
2022-06-28 03:41:43 -0700

    working on it!

--- src/Post.js
+++ src/Post.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import './Post.css';
 import Avatar from '@mui/material/Avatar';
+import VerifiedIcon from '@mui/icons-material/Verified';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import PublishIcon from '@mui/icons-material/Publish';
 
 function Post( displayName, userName, verified, text,image, avatar) {
   return (
     <div className='post'>
       <div className="post__avatar">
         <Avatar />
+      </div>
+      <div className="post__body">
+        <div className="post__header">
+          <div className="post__headerText">
+            <h3>Chialuka Prisca-mary Onuoha {" "} <span> <VerifiedIcon className='post__badge' /> @Lukas_T_noledge </span> </h3>
+          </div>
+          <div className="post__headerDescription">
+            <p>Build me a twitter clone</p>
+          </div>
+        </div>
+        <img src="https://media3.giphy.com/media/65ATdpi3clAdjomZ39/giphy.gif" alt="post image" />
+        <div className="post__footer">
+          <ChatBubbleOutlineIcon fontSize='small' />
+          <RepeatIcon fontSize='small' />
+          <FavoriteBorderIcon fontSize='small' />
+          <PublishIcon fontSize='small' />
+        </div>
       </div>
     </div>
   )
