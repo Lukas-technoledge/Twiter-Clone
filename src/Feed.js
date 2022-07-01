169edc7d4744f3bcb73059e78989e3d6d301c0d5
Lukas-technoledge <chialukaonuoha@gmail.com>
2022-07-01 10:02:45 -0700

    I'm done for today

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Feed.css';
 import TweetBox from './TweetBox';
 import Post from './Post'
+import db from './firebase';
 
 function Feed() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot =(snapshot => (
+      setPosts(snapshot.docs.map(doc => doc.data()))
+    ))
+  }, [])
+
   return (
     <div className='feed'>
       <div className="feed__header">
@@ -12,22 +21,19 @@ function Feed() {
 
       <TweetBox />
 
-      {/* post*/}
-      <Post displayName = 'Chialuka Prisca-mary Onuoha'
-       userName = 'Lukas_T_noledge'
-       verified = {true}
-       text = 'I am the greatest' 
-      image = 'https://media3.giphy.com/media/65ATdpi3clAdjomZ39/giphy.gif'
-       avatar = '/static/images/avatar/3.jpg' />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {posts.map(post => (
+       <Post 
+        displayName = {post.displayName}
+       userName = {post.userName}
+       verified = {post.verified}
+       text = {post.text} 
+       image = {post.image}
+       avatar = {post.avatar}
+      /> 
+      ))}
       
     </div>
-  )
+  );
 }
 
-export default Feed
+export default Feed;
